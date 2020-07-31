f5b2828b6abb14fd71f327f687757f294be94376
Ryan Gamel <ryan.gamel@gmail.com>
2020-07-31 13:55:22 -0500

    revert webpack config to es5

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,20 @@
-export const target = 'web';
-export const mode = 'development';
-export const entry = './src/index.tsx';
-export const output = {
-  path: require('path').resolve(__dirname, 'build'),
-  filename: 'bundle.js',
-};
-export const module = {
-  rules: [
-    {
-      test: /\.tsx?/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-    }
-  ]
-};
-export const resolve = { extensions: ['.ts', '.tsx', '.js'] };
-export const devServer = { contentBase: './', port: 5000 };
+module.exports = {
+  target: 'web',
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: require('path').resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      }
+    ]
+  },
+  resolve: { extensions: [ '.ts', '.tsx', '.js' ] },
+  devServer: { contentBase: './', port: 5000 }
+} 
